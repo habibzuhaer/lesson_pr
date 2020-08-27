48442197535f95c7003f0235398d047c3008dcee
Habib <habibzuhaer@gmail.com>
2020-08-27 22:59:50 +0300

    file Multidimensional_arrays create arrays 2 two

--- src/Multidimensional_arrays.java
+++ src/Multidimensional_arrays.java
@@ -7,9 +7,14 @@ public class Multidimensional_arrays {
         int[][] matrice = {{1,2,3},{4,5,6},{7,8,9}};
         System.out.println(matrice[2][2]);
         System.out.println(matrice[0][1]);
-        int[][] matrice2 = {{1,2,3},{4,5,6},{7,8,9}};
-        for(int i= 0;i<matrice2.length;i++);{
-            for(int j = 0;j<matrice2.length;j++);
+        int[][] matrice2 = {{5,5,7},{1,3,7},{8,12,9}};
+        /////
+        for(int i= 0;i<matrice2.length;i++)
+        {
+            for (int j = 0; j < matrice2.length; j++)
+            {
+                System.out.println(matrice2[i][j] + " ");
+            }
+            System.out.println();
         }
-    }
-}
+}}
