2a82f93cdcb896c84678d1d77179fb46005d32c5
Habib <habibzuhaer@gmail.com>
2020-09-01 22:57:57 +0300

    create lesson20 edit constructor

--- src/Lesson20.java
+++ src/Lesson20.java
@@ -1,6 +1,6 @@
 public class Lesson20 {
     public static void main(String[] args) {
-        Human human1 = new Human();
+        Human2 human1 = new Human2();
         human1.setName("Bob");
         human1.setAge(19);
     }
@@ -8,6 +8,15 @@ public class Lesson20 {
 class Human2 {
     private  String name;
     private  int age;
+
+    public Human2() {
+        System.out.println("Привет из первого конструктора !");
+    }
+    public Human2(String name){
+        System.out.println("Привет из второго конструктора !");
+        this.name =name;
+    }
+
     public void setName(String name) {this.name = name;}
     public void setAge(int age) {this.age = age;}
 }
