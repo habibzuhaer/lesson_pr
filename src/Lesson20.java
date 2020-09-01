afaa51080cc663a96fd6b3c0e7e4a02bbde66584
Habib <habibzuhaer@gmail.com>
2020-09-01 22:40:45 +0300

    create lesson20

---
+++ src/Lesson20.java
@@ -0,0 +1,11 @@
+public class Lesson20 {
+    public static void main(String[] args) {
+
+    }
+}
+class Human2 {
+    private  String name;
+    private  int age;
+    public void setName(String name) {this.name = name;}
+    public void setAge(int age) {this.age = age;}
+}
