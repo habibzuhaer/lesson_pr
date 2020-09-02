da2f1f76adbdb21f019ccb5c2e64ae99e27ee309
Habib <habibzuhaer@gmail.com>
2020-09-02 22:41:39 +0300

    create Constructor_Example

---
+++ src/Constructor_Example.java
@@ -0,0 +1,11 @@
+public class Constructor_Example {
+    void ConstructorExample(){
+        System.out.println("run");
+
+    }
+}
+class Run {
+    public static void main(String[] args) {
+        Constructor_Example constructorExample = new Constructor_Example();
+    }
+}
