bcfc0ee274c66eceb84096b4bae781d7a9ff8f13
Habib <habibzuhaer@gmail.com>
2020-09-02 22:36:35 +0300

    edit constructor create class Humans

--- src/Lesson20.java
+++ src/Lesson20.java
@@ -1,20 +1,32 @@
+
 public class Lesson20 {
     public static void main(String[] args) {
-        Human2 human1 = new Human2("Bob");    // - параметры выбирают конструктор
+        Humans human1 = new Humans("Bob" ,19);    // - параметры выбирают конструктор
         human1.setName("Bob");
         human1.setAge(19);
     }
 }
-class Human2 {
+class Humans {
     private  String name;
     private  int age;
 
-    public Human2() {
+   // public Human() {
+    //    this.name = "Имя по умолчанию";
+    //    this.age = 19;
+   //}
+
+    public Humans() {
         System.out.println("Привет из первого конструктора !");
     }
-    public Human2(String name){
+    public Humans(String name){
         System.out.println("Привет из второго конструктора !");
         this.name =name;
+    }
+
+    public Humans(String name, int age) {
+        System.out.println("Привет из третьего конструктора !");
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {this.name = name;}
