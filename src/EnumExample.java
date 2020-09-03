6dea7736784fe552989e5a2566b5a0205a74fe68
Habib <habibzuhaer@gmail.com>
2020-09-03 23:43:05 +0300

    create EnumExample edit file

--- src/EnumExample.java
+++ src/EnumExample.java
@@ -1,3 +1,14 @@
 public class EnumExample {
-    enum CoffeSize {SMALL, MEDIUM,BIG};
+    enum CoffeSize {SMALL,MEDIUM,BIG};
+
+
+    public static void main(String[] args) {
+        CoffeSize coffeSize = CoffeSize.BIG;
+        System.out.println(coffeSize);
+    }
 }
+//class CoffeSize2{
+   // public static final CoffeSize2 SMALL = new CoffeSize2();
+   // public static final CoffeSize2 MEDIUM = new CoffeSize2();
+   // public static final CoffeSize2 BIG = new CoffeSize2();
+//}
