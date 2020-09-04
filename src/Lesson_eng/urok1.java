4d31935b64e01e8f376695f6a95dd6165ac9851d
Habib <habibzuhaer@gmail.com>
2020-09-04 21:23:36 +0300

    create packege lesson_eng urok1.java

---
+++ src/Lesson_eng/urok1.java
@@ -0,0 +1,19 @@
+package Lesson_eng;
+
+public class urok1 {
+    /*
+    Nice to meet you
+    Nice to meet you, too
+    How are you
+    God morning
+    What lovely day
+
+
+
+
+
+
+
+
+     */
+}
