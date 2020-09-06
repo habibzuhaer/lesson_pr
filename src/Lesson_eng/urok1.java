64bc06ecc05746308c9f765b46049907472139eb
Habib <habibzuhaer@gmail.com>
2020-09-06 22:02:15 +0300

    create packege lesson_eng urok1.java

--- src/Lesson_eng/urok1.java
+++ src/Lesson_eng/urok1.java
@@ -7,6 +7,18 @@ public class urok1 {
     How are you
     God morning
     What lovely day
+    God aftermoon
+    A cup of tea , please
+    Have a nice day
+    See you later
+    God night
+    Sleep Well
+    an ambrella
+    have/ hav
+    vase
+    long tail
+
+
 
 
 
