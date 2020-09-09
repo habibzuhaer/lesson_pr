6748838151c6a6dacad98a1ae301a7ea40b4072c
Habib <habibzuhaer@gmail.com>
2020-09-09 22:20:18 +0300

    create/edit packege Solution lev04_04.java

---
+++ src/Javarush/SolutionLev04_04.java
@@ -0,0 +1,18 @@
+package Javarush;
+
+
+    public class SolutionLev04_04 {
+        private String fullName = "Bober";
+
+        public void setName(String firstName, String lastName) {
+            String fullName = firstName + " " + lastName;
+
+            //напишите тут ваш код
+            this.fullName = fullName;
+        }
+
+        public static void main(String[] args) {
+
+        }
+    }
+
