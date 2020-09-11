80bb066f807c64f52e7b4982cad7984e1c7e5b6d
Habib <habibzuhaer@gmail.com>
2020-09-11 21:06:11 +0300

    createSolutionLev04_05

---
+++ src/Lesson_eng/urok3.java
@@ -0,0 +1,67 @@
+package Lesson_eng;
+
+public class urok3 {
+    /*
+    A cup of tea
+    Apple
+    Bike
+    Book
+    Bus
+    Bye !
+    Coffee
+    Day
+    Fine
+    Good aftermoon !
+    Good moning !
+    Good night !
+    Have anice day !
+    Hello !
+    House
+    How are you ?
+    laptop
+    Lovely
+    Name
+    Nice to meet you
+    Phone
+    Photo
+    Please
+    Sandwich
+    See you leter !
+    Sleep well !
+    Surname
+    Umbrella
+    Very well
+    Watch
+    sugar
+
+     */
+    public static void main(String[] args) {
+        /*
+        I have a dog , but i have  no cat !                    dog - собака      but - но
+        I have a kitten , but i have no squirrel !             Cat - кошка
+        I have a cut , but i have no spoon .                   Kitten - котенок
+        I have a spoon , but i have no jam                     Squirrel - белка
+        I have a vase . I have no lamp .                       Spoon - ложка
+        I have file copybooks .I have no pen .                 Jam   - варенье
+                                                               Vase -  ваза
+        Give me two spoons !                                   Lamp - лампа
+                                                               Copy books  - тетрадь
+                                                               Notebook -   Тетрадь
+                                                               Pen  -- ручка
+                                                               Give me ! - дАй мне
+         */
+        class lecture {
+            /*
+            This                                                  Это
+            That -                                                Тот
+            This in an umbrella  -                                Это зонтик
+            This boy has a ball. He has a pistol, too .
+               У этого мальчика есть мяч ю У него также есть пистолет
+            Thas boy has agun . He has a stick , too .
+               У этого мальчика есть ружье . У него также есть палка
+            This boy has no book .                                У этого мальчика нет книги
+            Ерфе ищн рфы тщ пгт                                   У этого мальчика нет ружья
+             */
+        }
+    }
+}
