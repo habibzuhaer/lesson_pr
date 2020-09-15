98b1f2808a3f3583c1888f71f3cea31c89ebeb8d
Habib <habibzuhaer@gmail.com>
2020-09-15 22:37:45 +0300

    creater / edit SolutionLev04_09

---
+++ src/Javarush/SolutionLev04_09.java
@@ -0,0 +1,28 @@
+package Javarush;
+
+public class SolutionLev04_09 {
+    public static void main(String[] args) {
+        checkSeason(12);
+        checkSeason(4);
+        checkSeason(7);
+        checkSeason(10);
+    }
+
+    public static void checkSeason(int month) {
+        //напишите тут ваш код
+        // int month = a;
+        if (month < 3 || month == 12) {
+            System.out.println("зима");
+        }
+        else if (month >=3 && month <=5){
+            System.out.println("весна");
+        }
+        else if (month >=6 && month <=8){
+            System.out.println("лето");
+        }
+        else if (month > 8 && month <=11){
+            System.out.println("осень");
+        }
+
+    }
+}
