c862fad86bb8ce91b41b07a761088ee2d2355563
Habib <habibzuhaer@gmail.com>
2020-09-23 20:36:42 +0300

    creater / edit SolutionLev04_10

---
+++ src/Lesson_eng/urok4.java
@@ -0,0 +1,27 @@
+package Lesson_eng;
+
+public class urok4 {
+    /*
+    They have a hospital in the centre jf thecity
+    To unit -соединять
+    United - соединительный
+    my friend is from Beijing - мой друг из Пекина
+    on holiday
+    They are married - мы женаты
+    To be married - быть женатым
+    To marry - женится
+    Oh no Look at the weather
+    awtul - ужасная погода
+    great - прекрасный
+    really good - реально хорошо
+    right -прав
+    amazing - интересный
+    To build - строить
+
+
+
+
+
+
+     */
+}
