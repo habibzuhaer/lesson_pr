f636083ecabd0dce567f652ee1abab74a86649dc
Habib <habibzuhaer@gmail.com>
2020-09-23 22:31:05 +0300

    creater urok4

---
+++ src/Javarush/SolutionLev04_11.java
@@ -0,0 +1,15 @@
+package Javarush;
+import java.io.*;
+import java.util.*;
+public class SolutionLev04_11 {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+     //   String str = reader.readLine();
+        int year = Integer.parseInt(reader.readLine());
+        if (year % 400 != 0 & year % 100 != 0 | year % 4 != 0)
+            System.out.println("количество дней в году: 365");
+        else
+            System.out.println("количество дней в году: 366");
+    }
+}
