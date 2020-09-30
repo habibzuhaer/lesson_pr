fc3e4984092b3ee2a9a237296bea3ca167b933b3
Habib <habibzuhaer@gmail.com>
2020-09-30 20:58:11 +0300

    create /edit SolutionLev04_13.java

--- src/Javarush/SolutionLev4_13.java
+++ src/Javarush/SolutionLev4_13.java
@@ -1,4 +1,18 @@
 package Javarush;
+import java.util.Scanner;
 
 public class SolutionLev4_13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        if (a == b && a == c) {
+            System.out.println(a + " " + b + " " + c);
+        } else if (a == b || a == c) {
+            System.out.println(a + " " + a + " ");
+        } else if (b == a || b == c) {
+            System.out.println(b + " " + b + " ");
+        }
+    }
 }
