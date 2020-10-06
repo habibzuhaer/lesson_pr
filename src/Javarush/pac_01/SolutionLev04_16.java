0908d7eff28efe36fd4dea388c98e7b82bfeb9ac
Habib <habibzuhaer@gmail.com>
2020-10-06 21:11:25 +0300

    create SolutionLev04_15 / 16

---
+++ src/Javarush/pac_01/SolutionLev04_16.java
@@ -0,0 +1,9 @@
+package Javarush.pac_01;
+import java.util.*;
+public class SolutionLev04_16 {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
