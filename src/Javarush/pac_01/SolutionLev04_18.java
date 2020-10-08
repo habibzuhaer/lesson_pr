bf6fe344e02439e05c4f99ed882493207c1c368e
Habib <habibzuhaer@gmail.com>
2020-10-08 20:10:34 +0300

    create SolutionLev04_18

---
+++ src/Javarush/pac_01/SolutionLev04_18.java
@@ -0,0 +1,12 @@
+package Javarush.pac_01;
+import java.util.*;
+public class SolutionLev04_18 {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int age = sc.nextInt();
+        if (age > 20 )
+            System.out.println("И 18-ти достаточно");
+    }
+    }
