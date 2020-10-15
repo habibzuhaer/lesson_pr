e02fd01580fb6cd974d8a60fb33112532e43922a
Habib <habibzuhaer@gmail.com>
2020-10-15 22:59:05 +0300

    create SolutionLev04_4

---
+++ src/Javarush/pac_02/SolutionLev04_4.java
@@ -0,0 +1,13 @@
+package Javarush.pac_02;
+
+public class SolutionLev04_4 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int i = 10;
+        while (i >0){
+            System.out.println(i);
+            i--;
+        }
+
+    }
+}
