af7c43726b478535cf50f79e840f4f3ecf183182
Habib <habibzuhaer@gmail.com>
2020-10-19 20:18:03 +0300

    create SolutionLev04_10 /11

---
+++ src/Javarush/pac_02/SolutionLev04_11.java
@@ -0,0 +1,13 @@
+package Javarush.pac_02;
+
+public class SolutionLev04_11 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        for (int i = 1;i <=9;i++) {
+            System.out.print(" " + 8);
+        }
+        for (int j = 0;j <=10;j++) {
+            System.out.println(8);
+        }
+    }
+}
