f45049c88333e0e53ba12d72c87ffbc07be2cca3
Habib <habibzuhaer@gmail.com>
2020-10-20 22:11:54 +0300

    create SolutionLev04_12

---
+++ src/Javarush/pac_02/SolutionLev04_12.java
@@ -0,0 +1,10 @@
+package Javarush.pac_02;
+
+public class SolutionLev04_12 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String text = "Я никогда не буду работать за копейки. Амиго";
+        for (int i =0;i<10;i++)
+            System.out.println(text);
+    }
+}
