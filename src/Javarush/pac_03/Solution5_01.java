3191364b05c970beb55b61b9e25f370e3a8242a3
Habib <habibzuhaer@gmail.com>
2020-10-23 22:47:10 +0300

    create SolutionLev04_14

---
+++ src/Javarush/pac_03/Solution5_01.java
@@ -0,0 +1,15 @@
+package Javarush.pac_03;
+
+//public class Solution5_01 {
+    public class Solution5_01{
+        //напишите тут ваш код
+        String name = "Vaska";
+        int age = 8;
+        int weight = 4;
+        int strength = 2;
+
+        public static void main(String[] args) {
+
+        }
+    }
+//}
