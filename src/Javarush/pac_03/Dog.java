fe86996c5d472053edf05fd50339c33035026bdf
Habib <habibzuhaer@gmail.com>
2020-10-26 23:02:20 +0300

    create Solution05_03 / Dog.java

---
+++ src/Javarush/pac_03/Dog.java
@@ -0,0 +1,28 @@
+package Javarush.pac_03;
+
+public class Dog {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    //напишите тут ваш код
+    String name ;
+    int age;
+
+
+    public static void main(String[] args) {
+
+    }
+}
