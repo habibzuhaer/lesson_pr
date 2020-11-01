0ce406d1067361d939908a1256b8ec7ed3c628e9
Habib <habibzuhaer@gmail.com>
2020-11-01 22:03:55 +0100

    create Solution05_06

---
+++ src/Javarush/pac_03/Person.java
@@ -0,0 +1,13 @@
+package Javarush.pac_03;
+
+public class Person {
+    //напишите тут ваш код
+    String name;
+    int age ;
+    String address;
+    char sex;
+
+    public static void main(String[] args) {
+
+    }
+}
