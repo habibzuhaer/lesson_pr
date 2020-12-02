a4cf84ca81facdf05116b19268376f48c31147aa
Habib <habibzuhaer@gmail.com>
2020-12-02 23:17:44 +0100

    create Circles

---
+++ src/Javarush/pac_04/Circles.java
@@ -0,0 +1,24 @@
+package Javarush.pac_04;
+
+public class Circles {
+
+        public double x;
+        public double y;
+        public double radius;
+
+        //напишите тут ваш код
+        public Circles(){}
+        public Circles(double x){
+            this.x = x;
+        }
+        public Circles(double x, double y){
+            this.x = x;
+            this.y = y;
+        }
+
+        public Circles(double x, double y, double radius){
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+        }
+    }
