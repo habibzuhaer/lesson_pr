59ecaccec6ac8c6874c02a5b10b050aa0ccb2227
Habib <habibzuhaer@gmail.com>
2020-12-02 22:46:55 +0100

    create Rectangle

---
+++ src/Javarush/pac_04/Rectangle.java
@@ -0,0 +1,36 @@
+package Javarush.pac_04;
+
+public class Rectangle {
+    int top;
+    int left;
+    int width;
+    int height;
+    public Rectangle(int top)
+    {
+        this.top = top;
+    }
+    public Rectangle(int top, int left)
+    {
+        this.top = top;
+        this.left = left;
+    }
+    public Rectangle(int top, int left, int width)
+    {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+    }
+    public Rectangle(int top, int left, int width, int height)
+    {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
