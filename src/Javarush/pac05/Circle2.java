059e1c8275081ae742a43eded645e8d44e565843
Habib <habibzuhaer@gmail.com>
2020-12-28 22:43:33 +0100

    create pack05/Circle2

---
+++ src/Javarush/pac05/Circle2.java
@@ -0,0 +1,18 @@
+package Javarush.pac05;
+
+public class Circle2 {
+    public double x;
+    public double y;
+    public double r;
+    public Circle2(double x, double y, double r){
+        this.x= x;
+        this.y = y;
+        this.r = r;
+    }
+    //напишите тут ваш код
+    public static void main(String[] args) {
+
+    }
+}
+
+
